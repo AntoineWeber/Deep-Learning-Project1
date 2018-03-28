4181112b664be3f618c0cc97ee6c28e32d566b72
Cactusthorn <thibault.wildi@epfl.ch>
2018-03-28 22:03:00 +0200

    Added module data.py
    data.py is a modification of the provided module dlc_bci.py made to work booth with Tensorflow and Pycharm. Changes where made to .gitignore to ignore the folder ./data and ./data_bci in which the data is stored because it is very large (>100 MB).

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Deep-Learning
+
+## Tensorflow vs Pytorch
+
+When loading data using the data.py module, it is important to know whether 
+Tensorflow or Pytorch is used. Tensorflow uses NHWC for it's batch tensor 
+layout (i.e. batch.size() = [batch_size, height, width, nb_channels]) an 
+Pytorch used NCHW. Specify the layout using the 'nwhc' option while calling 
+the load function. 
---
+++ data.py
@@ -0,0 +1,96 @@
+import numpy as np
+import os
+import errno
+
+from six.moves import urllib
+
+_default_root_url = 'https://documents.epfl.ch/users/f/fl/fleuret/www/data/bci'
+
+
+def array_from_file(root, filename, base_url=_default_root_url) -> np.ndarray:
+    """
+    Returns a numpy array read from a File.
+
+    :param str root: The path to the root data folder and location of the file.
+    :param str filename: Complete name of the file to read.
+    :param str base_url: Root URL where the files can be found.
+    :return: An array loaded from file.
+    :rtype: numpy.array
+    """
+
+    file_path = os.path.join(root, filename)
+
+    if not os.path.exists(file_path):
+        try:
+            os.makedirs(root)
+        except OSError as e:
+            if e.errno == errno.EEXIST:
+                pass
+            else:
+                raise
+
+        url = base_url + '/' + filename
+
+        print('Downloading ' + url)
+        data = urllib.request.urlopen(url)
+        with open(file_path, 'wb') as f:
+            f.write(data.read())
+
+    return np.loadtxt(file_path)
+
+
+def load(root, train=True, download=True, one_khz=False, nhwc=False):
+    """
+    Args:
+
+        root (string): Root directory of dataset.
+
+        train (bool, optional): If True, creates dataset from training data.
+
+        download (bool, optional): If True, downloads the dataset from the internet and
+            puts it in root directory. If dataset is already downloaded, it is not
+            downloaded again.
+
+        one_khz (bool, optional): If True, creates dataset from the 1000Hz data instead
+            of the default 100Hz.
+
+        nhwc (bool, optional): If True, the layout of the returned tensor is
+            nhwc (Batch, Height, Width, Chanel) which is the convention in
+            Tensorflow, otherwise, BCHW is used, which is the convention in
+            Pytorch. Default is False (Pytorch).
+    """
+
+    nb_electrodes = 28
+
+    if train:
+
+        if one_khz:
+            dataset = array_from_file(root, 'sp1s_aa_train_1000Hz.txt')
+        else:
+            dataset = array_from_file(root, 'sp1s_aa_train.txt')
+
+        input = dataset[:, 1:].reshape(dataset.shape[0], nb_electrodes, -1)
+        target = dataset[:, 0]
+
+    else:
+
+        if one_khz:
+            input = array_from_file(root, 'sp1s_aa_test_1000Hz.txt')
+        else:
+            input = array_from_file(root, 'sp1s_aa_test.txt')
+        target = array_from_file(root, 'labels_data_set_iv.txt')
+
+        input = input.reshape(input.shape[0], nb_electrodes, -1)
+
+    if nhwc:
+        input = input.swapaxes(1, 2)
+
+    return input, target
+
+
+# Test code
+if __name__ == "__main__":
+    load("./data", train=True, one_khz=False, nhwc=True)
+    load("./data", train=False, one_khz=False)
+    load("./data", train=True, one_khz=True)
+    load("./data", train=False, one_khz=True)
